0cb6fad67dbdeb113438d5fc8412823d2a2240d6
Tom Taylor <tom.taylor@outlook.com>
2015-08-14 07:31:34 -0500

    moved from WDI-Woodstock to public

---
+++ javascript/scripts.js
@@ -0,0 +1,21 @@
+
+$(document).ready(function(){
+
+
+  $('#form-name').on('submit', function(e){
+    e.preventDefault();
+
+    $.ajax({
+      method: 'get',
+      url: 'http://shakeitspeare.com/api/sentence',
+      success: function(sentenceData){
+        $('#my-line').text(sentenceData.sentence.replace(/[\.?!]/,""));
+      }
+    })
+
+    var name = $('#name').val();
+    $('#line2').text("with " + name + " in bed.")
+
+  });
+
+})
